00420afd53002ca8d03b8bec90ecfd9b8ce67eb1
钟华 <zhong.hua@yalla.com>
2025-01-17 20:40:01 +0800

    init

---
+++ src/env.d.ts
@@ -0,0 +1,14 @@
+/// <reference types="@rsbuild/core/types" />
+
+// declare global {
+//   interface Window {
+//     ace: AceAjax;
+//   }
+// }
+
+type $TabItem = {
+  name: string;
+  id: string;
+  active?: boolean;
+  enabled?: boolean;
+};
